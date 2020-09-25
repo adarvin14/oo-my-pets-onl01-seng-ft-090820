a66681077f779be622755eea21faedd069edb35b
Alex Darvin <amdarvin14@gmail.com>
2020-09-25 16:58:58 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -21,7 +21,6 @@ class Owner
   end
  
   def self.all
-    self
     @@all
   end
   
@@ -69,6 +68,6 @@ class Owner
   end
   
   def list_pets
-    "I have #{self.dogs.count} dog(s), and #{owner.cats.count} cat(s)."
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
 end
